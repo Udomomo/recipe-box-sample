b3fd050cd55ff6523b52537a541436a68ff85641
Udomomo <udomomo@gmail.com>
2025-12-04 09:36:05 +0900

    fix configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import {Routes} from '@angular/router';
-import { RecipeList } from './recipe-list/recipe-list';
-import { RecipeDetail } from './recipe-detail/recipe-detail';
-import { RecipeForm } from './recipe-form/recipe-form';
+import { RecipeList } from './recipe-list/recipe-list.component';
+import { RecipeDetail } from './recipe-detail/recipe-detail.component';
+import { RecipeForm } from './recipe-form/recipe-form.component';
 
 export const routes: Routes = [
     {
